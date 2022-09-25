e7be850f608bb39fa19b2ddfd9e37e69a8cbcbeb
rtaskayev <washingtonqwerty@gmail.com>
2022-09-25 23:16:27 +0200

    JavaCore-5. 3 - JSON parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,94 +4,77 @@ import java.util.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 public class Main {
 
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+    public static void main(String[] args) throws IOException, ParseException {
 
-        String fileName = "data.xml";
-        String outputJsonFileName = "data2.json";
+        String fileName = "new_data.json";
 
-        List<Employee> list = parseXML(fileName);
-        System.out.println("Parsed objects from XML - " + list);
+        String json = readString(fileName);
+        System.out.println("JSON String is - " + json);
 
-        String json = listToJson(list); // Converting list of objects to Json data
-        System.out.println("Json data - " + json);
+        List<Employee> list = jsonToList(json);
 
-        writeString(json, outputJsonFileName); // Write json data to file
+        printList(list);
+
 
     }
 
-    //Method to parse xml into list of objects
-    public static List<Employee> parseXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
+    public static void printList(List<Employee> list) {
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Employee [" + (i + 1) + "] = " + list.get(i));
+        }
+
+    }
+
+    public static String readString(String fileName) throws IOException {
+        FileInputStream fileInputStream = null;
+        String data="";
+        StringBuilder stringBuffer = new StringBuilder();
+        try{
+            fileInputStream=new FileInputStream(fileName);
+            int i;
+            while((i=fileInputStream.read())!=-1)
+            {
+                stringBuffer.append((char)i);
+            }
+            data = stringBuffer.toString();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            if(fileInputStream!=null){
+                fileInputStream.close();
+            }
+        }
+        return data;
+    }
+
+    public static List<Employee> jsonToList(String json) throws ParseException {
 
         List<Employee> list = new ArrayList<>();
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(new File(fileName));
-        Node root = doc.getDocumentElement();
-        System.out.println("Root element: " + root.getNodeName());
-        list = read(root, list);
+        JSONParser parser = new JSONParser();
+        JSONArray array = (JSONArray) parser.parse(json);
 
-        return list;
-    }
+        for (int i = 0; i < array.size(); i++) {
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            JSONObject jsonObject = (JSONObject) array.get(i);
+            Employee employee = gson.fromJson(String.valueOf(jsonObject), Employee.class);
 
-    // Method to read xml document and parse into list of objects
-    private static List<Employee> read(Node node, List<Employee> empList) {
-
-        NodeList nodeList = node.getChildNodes();
-        List<Employee> list = empList;
-        Map<String, String> map_ = new HashMap<>();
-
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Node node_ = nodeList.item(i);
-            if (Node.ELEMENT_NODE == node_.getNodeType()) {
-                System.out.println("Name: " + node_.getNodeName());
-                Element element = (Element) node_;
-                NamedNodeMap map = element.getAttributes();
-                // Checking if node is employee and it has at least 1 value inside
-                if (node_.getNodeName().equals("employee") && map.getLength() > 0) {
-                    for (int a = 0; a < map.getLength(); a++) {
-                        String attrName = map.item(a).getNodeName();
-                        String attrValue = map.item(a).getNodeValue();
-                        System.out.println("Attribute: " + attrName + "; value: " + attrValue);
-                        map_.put(attrName, attrValue); // Inserting data into map
-                    }
-                    Employee emp = new Employee(map_); // Creating new employee object from created map
-                    list.add(emp);
-                    map_.clear();
-                }
-                read(node_, list);
-            }
+            list.add(employee);
         }
         return list;
     }
 
 
-    // Method to transfer list of objects to json
-    public static String listToJson(List<Employee> list) {
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-
-        return gson.toJson(list);
-    }
-
-    // Method to write json data into file
-    public static void writeString(String stringToWrite, String fileName) {
-        try (FileWriter file = new
-                FileWriter(fileName)) {
-            file.write(stringToWrite);
-            file.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
